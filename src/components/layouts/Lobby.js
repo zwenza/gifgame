aed48c9fd0e52e63af74b8541b51c2440aaf4c22
Florian Kapfenberger <florian.kapfenberger@me.com>
2017-01-05 19:11:15 +0100

    added Lobby creation UI

--- src/components/layouts/Lobby.js
+++ src/components/layouts/Lobby.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { Row, Col, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, ListGroup, ListGroupItem, Modal, Button } from 'react-bootstrap';
 import firebase from 'firebase';
+
+import Spinner from '../Spinner';
+
+import '../../styles/Lobby.css';
 
 class Lobby extends Component {
   constructor(){
     super();
-    this.state = { waitingUsers: [] };
+    this.state = {
+      waitingUsers: [],
+      otherPlayer: '',
+      showModal: false
+    };
   }
 
   componentDidMount(){
@@ -16,15 +24,62 @@ class Lobby extends Component {
     });
   }
 
+  listPlayers = () => {
+    const self = this;
+
+    if (this.state.waitingUsers.length === 0) {
+      return <Spinner />;
+    }
+
+    return this.state.waitingUsers.map((user, i) => <ListGroupItem key={i} onClick={() => self.selectPlayer(user)}>{user}</ListGroupItem>)
+  }
+
+  selectPlayer = (user) => {
+    this.setState({otherPlayer: user});
+    this.open();
+  }
+
+  createLobby = () => {
+    console.log(`TODO create Lobby with ${this.state.otherPlayer}`);
+    // TODO: go to lobby with player
+  }
+
+  // modal methods
+
+  close = () => {
+    this.setState({ showModal: false });
+  }
+
+  open = () => {
+    this.setState({ showModal: true });
+  }
+
   render(){
+
+    const modal = (
+      <Modal show={this.state.showModal} onHide={this.close}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Lobby with <strong>{this.state.otherPlayer}</strong></Modal.Title>
+        </Modal.Header>
+
+        <Modal.Footer>
+          <Button bsStyle="primary" onClick={this.createLobby}>Create Lobby</Button>
+          <Button bsStyle="danger" onClick={this.close}>Cancel</Button>
+        </Modal.Footer>
+
+      </Modal>
+    )
+
     return(
-      <Row>
+      <Row className='lobby' >
         <Col md={6} mdOffset={3}>
-          <h1>waiting users</h1>
+          <h1>Gamers in Lobby</h1>
           <ListGroup>
-          { this.state.waitingUsers.map(user => <ListGroupItem>{user}</ListGroupItem>) }
+            { this.listPlayers() }
           </ListGroup>
         </Col>
+
+        {modal}
       </Row>
     );
   }
